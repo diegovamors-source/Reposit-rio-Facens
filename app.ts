2791bbc8aedbbf10f854b5c5e0d39ac5ec8bbe74
Diego <diegovamors@gmail.com>
2026-09-18 20:16:18 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+import { MuralRecadosComponent } from './mural-recados/mural-recados';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MuralRecadosComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+}
